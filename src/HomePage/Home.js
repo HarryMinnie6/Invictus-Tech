5c9037c4f9b0acb11b00315ba237a16a9c216070
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-11-10 18:02:09 +0200

    started landing page

--- src/HomePage/Home.js
+++ src/HomePage/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <section>
-            home page
-        
-        </section>
-    )
+  return (
+    <section>
+      <div className="welcome__wrapper">
+        <h3>Welcome to</h3>
+        <h1>Invictus-Tech</h1>
+      </div>
+      <div className="partners__bar">
+        <div className="bar">test</div>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
