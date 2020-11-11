131178e3c4513635d2da6b878028ef6dbadd59da
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-11-11 17:13:59 +0200

    Finished about section

--- src/AboutPage/AboutPage.js
+++ src/AboutPage/AboutPage.js
@@ -8,18 +8,20 @@ function AboutPage() {
       <h1> ¿ What we do ?</h1>
       <div className="about-details__holder">
         <div className="about-details__description">
-          <div className="about-details__paragraph-holder">
-            <p className="about-details__paragraph">
-              At Invictus Tech, we use technology as an enabler and the
-              connected device as a vehicle to deliver IoT based solutions that
-              assist our Customers to achieve their business goals. We work with
-              our Invictus Tech Trusted Partner Network (TPN) to design and
-              implement customised, security-enhanced solutions that integrate
-              seamlessly and rapidly into our Customers’ operating processes.
-            </p>
-          </div>
+          <p className="about-details__paragraph">
+            At Invictus Tech, we use technology as an enabler and the connected
+            device as a vehicle to deliver IoT based solutions that assist our
+            Customers to achieve their business goals. We work with our Invictus
+            Tech Trusted Partner Network (TPN) to design and implement
+            customised, security-enhanced solutions that integrate seamlessly
+            and rapidly into our Customers’ operating processes.
+          </p>
+
           <div className="about-details__img-holder">
-            <div className="img__holder"></div>
+            <img
+              src="https://cdn.pixabay.com/photo/2018/10/02/21/39/smart-3720021_960_720.jpg"
+              alt="img"
+            />
           </div>
         </div>
       </div>
--- src/LandingPage/PartnersBar.js
+++ src/LandingPage/PartnersBar.js
@@ -3,18 +3,16 @@ import "./PartnersBar.css";
 
 function PartnersBar() {
   return (
-    <section className="partners__bar">
+    <div className="partners__bar">
       <div className="partners__holder">
-        <div>
-          <div className="partner one"></div>
-          <div className="partner two"></div>
-          <div className="partner three"></div>
-          <div className="partner four"></div>
-          <div className="partner five"></div>
-          <div className="partner six"></div>
-        </div>
+        <div className="partner one"></div>
+        <div className="partner two"></div>
+        <div className="partner three"></div>
+        <div className="partner four"></div>
+        <div className="partner five"></div>
+        <div className="partner six"></div>
       </div>
-    </section>
+    </div>
   );
 }
 
