992c490566dc6392c6cf849856c549a205e52bb4
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-11-13 12:56:44 +0200

    Finished services display 90%

--- src/Services/Services.js
+++ src/Services/Services.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Services.css";
 function Services() {
   return (
-    <section className="services__page">
+    <section className="services__page" id="services">
       <h1> ¿ What we do ?</h1>
       <div className="services-details__holder">
         <div className="services-details__description">
@@ -16,13 +16,36 @@ function Services() {
               implement customised, security-enhanced solutions that integrate
               seamlessly and rapidly into our Customers’ operating processes.
             </p>
+            <a href="#">Learn More</a>
           </div>
-
           <div className="services-details__img-holder">
             <img
               src="https://cdn.pixabay.com/photo/2018/10/02/21/39/smart-3720021_960_720.jpg"
               alt="img"
             />
+          </div>
+        </div>
+      </div>
+      {/*----------------------------------------------------------  */}
+      <div className="services-details__holder">
+        <div className="services-details__description2">
+          <div className="services-details__img-holder2">
+            <img
+              src="https://cdn.pixabay.com/photo/2018/10/02/21/39/smart-3720021_960_720.jpg"
+              alt="img"
+            />
+          </div>
+          <div className="services-details__paragraph2">
+            <h3>Consulting</h3>
+            <p>
+              At Invictus Tech, we use technology as an enabler and the
+              connected device as a vehicle to deliver IoT based solutions that
+              assist our Customers to achieve their business goals. We work with
+              our Invictus Tech Trusted Partner Network (TPN) to design and
+              implement customised, security-enhanced solutions that integrate
+              seamlessly and rapidly into our Customers’ operating processes.
+            </p>
+            <a href="#">Learn More</a>
           </div>
         </div>
       </div>
